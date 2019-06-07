72ea10629075657d45528d706a3192088976c828
Keith Cirkel <keithamus@users.noreply.github.com>
2019-06-07 15:20:25 +0100

    style: fix lint errors

--- test/index.ts
+++ test/index.ts
@@ -10,10 +10,10 @@ debounce(() => {}, 100, {start: false})
 debounce(() => {}, 100, {middle: false})
 
 const throttled = throttle(() => {})
- // no args, returns void
+// no args, returns void
 const result: void = throttled()
 console.log(result)
- // no args, returns void
+// no args, returns void
 const result2: void = throttled.cancel()
 console.log(result2)
 
@@ -21,6 +21,6 @@ const throttledWithArgs = throttle((n: number, t: string) => t.slice(n))
 // needs args of number and string, returns void
 const result3: void = throttledWithArgs(1, '123')
 console.log(result3)
- // no args, returns void
+// no args, returns void
 const result4: void = throttledWithArgs.cancel()
 console.log(result4)
